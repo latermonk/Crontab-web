c70d40377df44f66094d24401dcc5c06500c2060
latermonk <iback.china@gmail.com>
2021-08-30 16:09:04 +0800

    change  directory

---
+++ docs/build/precache-manifest.65ade5ff83c87e088320d6f62d573f80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c336e4277110365d4f95",
+    "url": "/Crontab-web/static/css/main.63eb6933.chunk.css"
+  },
+  {
+    "revision": "c336e4277110365d4f95",
+    "url": "/Crontab-web/static/js/main.c336e427.chunk.js"
+  },
+  {
+    "revision": "23a3ff56a223555d4697",
+    "url": "/Crontab-web/static/js/1.23a3ff56.chunk.js"
+  },
+  {
+    "revision": "8685d1b08fa929f6a165",
+    "url": "/Crontab-web/static/js/runtime~main.8685d1b0.js"
+  },
+  {
+    "revision": "60a2c6b4b4b2c4150e2d2de2551b7523",
+    "url": "/Crontab-web/index.html"
+  }
+];
