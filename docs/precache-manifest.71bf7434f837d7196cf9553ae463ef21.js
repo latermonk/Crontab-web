e7434ef619935dd3130916429b1df274e886c9aa
latermonk <iback.china@gmail.com>
2021-08-30 15:43:54 +0800

    build to docs

---
+++ docs/precache-manifest.71bf7434f837d7196cf9553ae463ef21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68644c94c90cfbf64280",
+    "url": "/static/css/main.63eb6933.chunk.css"
+  },
+  {
+    "revision": "68644c94c90cfbf64280",
+    "url": "/static/js/main.68644c94.chunk.js"
+  },
+  {
+    "revision": "10509946bcade3253162",
+    "url": "/static/js/1.10509946.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2280a2a3788cc6de619ad5ae0f6daf54",
+    "url": "/index.html"
+  }
+];
